b05a63850772091ef6dab8d5914c65dcfc644342
Rachiit20 <rachhitk2004@gmail.com>
2025-08-23 21:15:00 +0530

    footer

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -1,11 +1,20 @@
 
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className='bg-gray-900 md:px-36 text-left w-full mt-10'>
+      <div className=''>
+          <div>
+            <img src={assets.logo_light} alt="logo" />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem tenetur, numquam et deleniti rerum quod nesciunt ipsa illum excepturi aut.</p>
+          </div>
+          <div></div>
+          <div></div>
+      </div>
+      <p>Copyright 2025 &copy; Learnyfy </p>
+    </footer>
   )
 }
 
