67184fb5d481fbdce8a3428d9b2a971671c053fc
Rachiit20 <rachhitk2004@gmail.com>
2025-09-28 20:04:26 +0530

    Toggle Lectures

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -10,6 +10,7 @@ const CourseDetails = () => {
   const {id} =useParams()
 
   const [courseData , setCourseData] = useState(null)
+  const [openSection , setopenSection] = useState({})
 
   const {allCourses , calculateRating , calculateNoOfLectures ,calculateCourseDuration ,calculateChapterTime } = useContext(AppContext)
 
@@ -23,6 +24,14 @@ const CourseDetails = () => {
   useEffect(() => {
     fetchCourseData()
   }, [allCourses, id])
+
+const toggleSection =(index)=>{
+  setopenSection((prev)=>(
+    {...prev ,
+      [index] : !prev[index],
+    }
+  ))
+}
 
   return  courseData ? (
     <>
@@ -54,7 +63,7 @@ const CourseDetails = () => {
                <div className='pt-5 '>
                 {courseData.courseContent.map((chapter ,index)=>(
                   <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
-                    <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
+                    <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={()=>toggleSection(index)}>
                       <div className='flex items-center gap-2'>
                         <img src={assets.down_arrow_icon} alt="arrow icon" />
                         <p className='font-medium md:text-base text-sm' >{chapter.chapterTitle}</p>
@@ -62,7 +71,7 @@ const CourseDetails = () => {
                       <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
                     </div>
 
-                    <div className='overflow-hidden transition-all duration-3000 max-h-96'>
+                    <div className={`overflow-hidden transition-all duration-300 ${openSection[index] ? 'max-h-96' : 'max-h-0'}`}>
                       <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
                         {chapter.chapterContent.map((lecture , index)=>(
                           <li key={index} className='flex items-start gap-2 py-1' >
