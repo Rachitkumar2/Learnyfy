8c911025c52c8b63b2f030066fdc32615b82b5d1
Rachit Kumar <rachhitk2004@gmail.com>
2025-10-04 23:10:15 +0530

    Add index.js for component exports to fix Vercel build

---
+++ client/src/components/student/index.js
@@ -0,0 +1,12 @@
+// Re-export all student components
+export { default as Hero } from './Hero.jsx';
+export { default as Companies } from './Companies.jsx';
+export { default as CourseSection } from './CourseSection.jsx';
+export { default as Testimonials } from './Testimonials.jsx';
+export { default as CallToAction } from './CallToAction.jsx';
+export { default as Footer } from './Footer.jsx';
+export { default as Loading } from './Loading.jsx';
+export { default as Navbar } from './Navbar.jsx';
+export { default as Searchbar } from './Searchbar.jsx';
+export { default as CourseCard } from './CourseCard.jsx';
+export { default as Rating } from './Rating.jsx';
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,9 +1,11 @@
-import Hero from '../../components/student/Hero';
-import Companies from '../../components/student/Companies';
-import CourseSection from '../../components/student/CourseSection';
-import Testimonials from '../../components/student/Testimonials';
-import CallToAction from '../../components/student/CallToAction';
-import Footer from '../../components/student/Footer';
+import {
+  Hero,
+  Companies,
+  CourseSection,
+  Testimonials,
+  CallToAction,
+  Footer
+} from '../../components/student';
 
 
 const Home = () => {
